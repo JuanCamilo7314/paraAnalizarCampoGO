d135f8ace2fa958cbb5d7099d8d520e04d4a9a79
Cristian David Escobar <cescobar@truora.com>
2023-03-09 11:17:33 -0500

    Add endpoint to API

--- main.go
+++ main.go
@@ -12,5 +12,9 @@ func main() {
 		return c.SendString("Hello, World from Samuel!")
 	})
 
+	app.Get("/cristian", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World from Cristian this is a Software project!")
+	})
+
 	app.Listen(":5000")
 }
