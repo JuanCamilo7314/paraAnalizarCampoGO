3221438f0fddeff0e433a8af8705019d5ec7d84c
Cristian David Escobar <cescobar@truora.com>
2023-04-04 16:13:11 -0500

    add repositori

--- src/services/test_service.go
+++ src/services/test_service.go
@@ -1,31 +1,9 @@
 package services
 
 import (
-	"AgroXpert-Backend/src/database"
-	"context"
-	"fmt"
-	"log"
-
-	"go.mongodb.org/mongo-driver/bson"
+	"AgroXpert-Backend/src/repositories"
 )
 
 func GetDataTest() {
-	collection := database.Db.GetCollection("FarmLot")
-	filter := bson.M{}
-
-	farms, err := collection.Find(context.Background(), filter)
-	if err != nil {
-		fmt.Println(err, "Error en el find")
-
-		return
-	}
-
-	for farms.Next(context.Background()) {
-		var result bson.M
-		err := farms.Decode(&result)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(result)
-	}
+	repositories.GetData()
 }
