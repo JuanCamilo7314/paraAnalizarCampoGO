191422bd874f2e5cc3b9a20c39453f356d81c3fc
Cristian David Escobar <cescobar@truora.com>
2023-04-15 00:39:21 -0500

    calculare estimate production

--- src/controllers/estimates_controller.go
+++ src/controllers/estimates_controller.go
@@ -2,26 +2,55 @@ package controllers
 
 import (
 	"AgroXpert-Backend/src/models"
-	"fmt"
-	"reflect"
+	"AgroXpert-Backend/src/services"
+	"errors"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func PostNewEstimate(c *fiber.Ctx) error {
-	var estimate models.EstimateModel
-	fmt.Println(reflect.TypeOf(c.Body()))
+	var estimateReq models.ReqEstimate
 
-	if err := c.BodyParser(&estimate); err != nil {
+	if err := c.BodyParser(&estimateReq); err != nil {
 		return c.Status(400).JSON(models.Response{
 			Success: false,
 			Message: err.Error(),
-			Data:    c.Body(),
 		})
 	}
 
-	return c.Status(200).JSON(fiber.Map{
-		"message": "Estimate created successfully",
-		"data":    estimate,
+	if err := validateEstimate(estimateReq); err != nil {
+		return c.Status(400).JSON(models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+	}
+
+	estimateResult, err := services.CreateEstimate(estimateReq)
+
+	if err != nil {
+		return c.Status(400).JSON(models.Response{
+			Success: false,
+			Message: err.Error(),
+		})
+	}
+
+	return c.Status(200).JSON(models.Response{
+		Success: true,
+		Message: "Estimate created successfully",
+		Data:    estimateResult,
 	})
+
+}
+
+func validateEstimate(estimateReq models.ReqEstimate) error {
+
+	if estimateReq.IdFarmLot == "" {
+		return errors.New("IdFarmLot is required")
+	}
+
+	if len(estimateReq.TreesAssessed) == 0 || estimateReq.TreesAssessed == nil {
+		return errors.New("TreesAssessed is required")
+	}
+
+	return nil
 }
--- src/models/estimates_model.go
+++ src/models/estimates_model.go
@@ -9,19 +9,19 @@ import (
 type EstimateModel struct {
 	ID                   primitive.ObjectID `json:"id" bson:"_id"`
 	Date                 time.Time          `json:"date" bson:"date"`
-	NumTrees             int64              `json:"numTrees" bson:"numTrees"`
-	TotalFruitsEstimates int64              `json:"totalFruitsEstimates" bson:"totalFruitsEstimates"`
-	AverageFruits        int64              `json:"averageFruits" bson:"averageFruits"`
-	EstimatedProduction  int64              `json:"estimatedProduction" bson:"estimatedProduction"`
+	NumTrees             int                `json:"numTrees" bson:"numTrees"`
+	TotalFruitsEstimates int                `json:"totalFruitsEstimates" bson:"totalFruitsEstimates"`
+	AverageFruits        float64            `json:"averageFruits" bson:"averageFruits"`
+	EstimatedProduction  int                `json:"estimatedProduction" bson:"estimatedProduction"`
 	TreesAssessed        []TreesAssessed    `json:"treesAssessed" bson:"treesAssessed"`
 }
 
 type TreesAssessed struct {
-	NumFruits    int64 `json:"numFruits"`
-	NumQuartiles int64 `json:"numQuartiles"`
+	NumFruits    int `json:"numFruits"`
+	NumQuartiles int `json:"numQuartiles"`
 }
 
 type ReqEstimate struct {
-	IdFarmLot     primitive.ObjectID `json:"idFarm" bson:"_id"`
-	TreesAssessed []TreesAssessed    `json:"treesAssessed" bson:"treesAssessed"`
+	IdFarmLot     string          `json:"idFarm"`
+	TreesAssessed []TreesAssessed `json:"treesAssessed"`
 }
