e2eb47c7769f6140ff57617034a71a938cd6aeb1
mcgp <51185772+mcgp@users.noreply.github.com>
2023-04-15 15:59:53 -0500

    model harvest

---
+++ src/models/harvest.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Harvest struct {
+	ID                    primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Type                  string             `bson:"type,omitempty" json:"type,omitempty"`
+	IDFarmLot             primitive.ObjectID `bson:"idFarmLot,omitempty" json:"idFarmLot,omitempty"`
+	EvaluationStartDate   time.Time          `bson:"evaluationStartDate,omitempty" json:"evaluationStartDate,omitempty"`
+	EvaluationEndDate     time.Time          `bson:"evaluationEndDate,omitempty" json:"evaluationEndDate,omitempty"`
+	Estimates             []primitive.ObjectID `bson:"estimates,omitempty" json:"estimates,omitempty"`
+	SummaryFinalProduction primitive.ObjectID `bson:"summaryFinalProduction,omitempty" json:"summaryFinalProduction,omitempty"`
+}
