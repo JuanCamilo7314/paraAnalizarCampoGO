f09123fb122e074b7694554454c57bd72c3ddcee
josamarb <sammy99marin@gmail.com>
2023-04-15 10:33:44 -0500

    Middleware Acces Control

--- src/main.go
+++ src/main.go
@@ -5,6 +5,7 @@ import (
 
 	"AgroXpert-Backend/src/configs"
 	"AgroXpert-Backend/src/database"
+	"AgroXpert-Backend/src/middlewares"
 	"AgroXpert-Backend/src/routes"
 )
 
@@ -12,7 +13,7 @@ func main() {
 	configs.InitEnv()
 	database.InitMongoConnection()
 	app := fiber.New()
-
+	app.Use(middlewares.AccessOriginAnywhere)
 	SetupRoutes(app)
 
 	app.Listen(":5000")
