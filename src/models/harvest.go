e13efd861400c9335634e09259ec6e735d2a412b
josamarb <sammy99marin@gmail.com>
2023-04-15 22:02:40 -0500

    Correccion

--- src/models/harvest.go
+++ src/models/harvest.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Harvest struct {
-	ID                    primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Type                  string             `bson:"type,omitempty" json:"type,omitempty"`
-	IDFarmLot             primitive.ObjectID `bson:"idFarmLot,omitempty" json:"idFarmLot,omitempty"`
-	EvaluationStartDate   time.Time          `bson:"evaluationStartDate,omitempty" json:"evaluationStartDate,omitempty"`
-	EvaluationEndDate     time.Time          `bson:"evaluationEndDate,omitempty" json:"evaluationEndDate,omitempty"`
-	Estimates             []primitive.ObjectID `bson:"estimates,omitempty" json:"estimates,omitempty"`
-	SummaryFinalProduction primitive.ObjectID `bson:"summaryFinalProduction,omitempty" json:"summaryFinalProduction,omitempty"`
+	ID                     primitive.ObjectID   `bson:"_id,omitempty" json:"id,omitempty"`
+	Type                   string               `bson:"type,omitempty" json:"type,omitempty"`
+	IDFarmLot              primitive.ObjectID   `bson:"idFarmLot,omitempty" json:"idFarmLot,omitempty"`
+	EvaluationStartDate    time.Time            `bson:"evaluationStartDate,omitempty" json:"evaluationStartDate,omitempty"`
+	EvaluationEndDate      time.Time            `bson:"evaluationEndDate,omitempty" json:"evaluationEndDate,omitempty"`
+	Estimates              []primitive.ObjectID `bson:"estimates ,omitempty" json:"estimates ,omitempty"`
+	SummaryFinalProduction primitive.ObjectID   `bson:"summaryFinalProduction,omitempty" json:"summaryFinalProduction,omitempty"`
 }
--- src/repositories/harvest_repository.go
+++ src/repositories/harvest_repository.go
@@ -75,6 +75,7 @@ func GetHarvestsByFarmLotID(FarmLotID string) ([]models.Harvest, error) {
 	}
 
 	for harvest.Next(context.Background()) {
+		// fmt.Printf("harvest: %v" + harvest.Current.String())
 		err := harvest.Decode(&modelHarvest)
 		if err != nil {
 			return nil, fmt.Errorf("error decode harvest: %v", err)
